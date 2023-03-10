574b08f88305cf67625c81c21a883352f69be20d
Doragon <moulaykadii2001@gmail.com>
2023-03-10 01:14:29 +0000

    Signup frontend request + backend handling

--- y/src/views/signup.jsx
+++ y/src/views/signup.jsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+import axiosClient from '../axios-client'
+import { useStateContext } from '../contexts/ContextProvider'
 
 export default function Signup() {
+
+  const nameRef = useRef()
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const confirmPasswordRef = useRef()
+  const [errors,setErrors] = useState(null)
+  const {setUser,setToken} = useStateContext()
+  const onSubmit = (e) => {
+    e.preventDefault()
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: confirmPasswordRef.current.value,
+    }
+    axiosClient.post('/signup',payload)
+    .then(({data})=>{
+      setUser(data.user) 
+      setToken(data.token)
+    })
+    .catch(err => {
+      const response = err.response
+      //console.log(err);
+      if (response && response.status === 422){
+        setErrors(response.data.errors) 
+      }
+    })
+    console.log(payload);
+  }
   return (
-    <div>S</div>
-  )
+    <div className='login-signup-form animated fadeInDown'>
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">
+            Sign up new account
+          </h1>
+          {errors && 
+            <div className='alert'>
+              {Object.keys(errors).map(key=>(
+                <p key={key}>{errors[key][0]}</p>
+              ))}
+              <p key={errors}></p>
+            </div>}
+          <input ref={nameRef} type="text" placeholder='Full name'/>
+          <input ref={emailRef} type="email" placeholder='Email'/>
+          <input ref={passwordRef} type="password" placeholder='Pasword'/>
+          <input ref={confirmPasswordRef} type="password" placeholder='Confirm password'/>
+          <button className="btn btn-block">Login</button>
+          <p className="message">
+            Login? <Link to="/login">Click here</Link>
+          </p>
+        </form>
+      </div>
+    </div>  
+    )
 }
--- y/src/views/users.jsx
+++ y/src/views/users.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export default function Users() {
+  // Remove token from local storage
+  localStorage.removeItem('ACCESS_TOKEN');
+
   return (
     <div>U</div>
   )
